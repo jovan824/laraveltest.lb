edb4aa52e001be78cf64ca1355cdd090482e3746
Jovan Simidian <jovansimidian@hotmail.com>
2021-12-14 18:07:52 +0200

    uploaded the project

---
+++ app/Models/Post.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post
+{
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+
+        $this->excerpt = $excerpt;
+    
+        $this->date = $date;
+    
+        $this->body = $body;
+
+        $this->slug = $slug;
+    }
+
+    public static function all()
+    {
+        $files = File::files(resource_path("posts/"));
+
+
+        return array_map(fn($files) => $files ->getContents(), $files);
+    }
+
+
+    public static function find($slug)
+    {
+        
+        if(!file_exists($path = resource_path("posts/{$slug}.html"))){
+
+            throw new ModelNotFoundException();
+
+        }
+
+
+
+        return cache()->remember("posts.{$slug}", 1200 ,fn() => file_get_contents($path));
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\File;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $files = File::files(resource_path("posts/"));
+
+    $posts = collect($files)
+    
+    ->map(function ($file) {
+
+        $document = YamlFrontMatter::parseFile($file); 
+
+        return new Post(
+
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+
+        );
+    });
+        
+
+  
+
+        
+
+
+    return view("posts", [
+
+        'posts' => $posts
+    ]);
+});
+
+Route::get('posts/{post}', function ($slug){
+
+
+    return view ('post', [
+        'post' => Post::find($slug)
+]);
+
+
+})->where('post','[A-z_\-]+');
